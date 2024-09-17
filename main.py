39d8ce0baeefde164382e25d32c25690834a3068
ivanovich1071 <pts.bryansk@mail.ru>
2024-09-17 15:56:39 +0300

    git_evn

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from handlers.start_handler import router as start_router
 from handlers.weather_handler import router as weather_router
 from handlers.guide_handler import router as guide_router
 from handlers.exit_handler import router as exit_router
-
+from services.db_service import create_db
 # Инициализация бота
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
@@ -17,6 +17,7 @@ dp.include_router(guide_router)
 dp.include_router(exit_router)
 
 async def main():
+    create_db()  # Создаем БД при запуске бота
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
